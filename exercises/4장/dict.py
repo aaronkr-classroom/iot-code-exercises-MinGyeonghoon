a2d99f1fc9cb5767f1eb857c6c50227a6451b784
MinGyeonghoon <khmin309@a.ut.ac.kr>
2026-04-05 14:55:25 +0900

    Add files via upload

---
+++ exercises/4장/dict.py
@@ -0,0 +1,11 @@
+sensors = {
+    'dht11': {
+        'temperature': 23,
+        'humidity': 47,
+        'unit': 'celsius'
+    },
+    'bh1750': {
+        'illuminance': 450,
+        'unit': 'lux'
+    }
+}
